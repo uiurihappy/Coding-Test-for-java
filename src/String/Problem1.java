697bc48f3f3e52ee1ef749970581384eb28ca3d8
ybchar <uiurihappy@sju.ac.kr>
2023-01-17 12:13:48 +0900

    First commit

---
+++ src/String/Problem1.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+class Main {
+
+	public int solution(String str, char t){
+		int answer = 0;
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		System.out.println(1);
+		System.out.println();
+	}
+}
