3838e1a782b67700b2ebfda165c34d41e5b3d9bc
ybchar <uiurihappy@sju.ac.kr>
2023-01-17 16:27:31 +0900

    feat: problem2 solve

--- src/String/Problem1.java
+++ src/String/Problem1.java
@@ -2,7 +2,7 @@ package String;
 
 import java.util.Scanner;
 
-class Main {
+class Problem1 {
 
 	public int solution(String str, char t){
 		int answer = 0;
@@ -10,17 +10,21 @@ class Main {
 		str = str.toUpperCase();
 		t = Character.toUpperCase(t);
 
-		for(int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == t) {
-				answer++;
-			}
+//		for(int i = 0; i < str.length(); i++) {
+//			if (str.charAt(i) == t) {
+//				answer++;
+//			}
+//		}
+
+		for (char x : str.toCharArray()) {
+			if (x == t) answer++;
 		}
 
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		Main T = new Main();
+		Problem1 T = new Problem1();
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
 		char t = kb.next().charAt(0);
---
+++ src/String/Problem2.java
@@ -0,0 +1,38 @@
+package String;
+
+import java.util.Scanner;
+
+class Problem2 {
+
+	public String solution(String str){
+		String answer = "";
+		int temp;
+
+//		for(int i =0; i < str.length(); i++){
+//			temp = (int)str.charAt(i);
+//
+//			if (temp >= 65 && temp <= 90) {
+//				answer += (char)(temp + 32);
+//			}else if (temp >= 97 && temp <= 122){
+//				answer += (char)(temp - 32);
+//			}else{
+//				answer += (char)(temp);
+//			}
+//		}
+
+		for (char x : str.toCharArray()) {
+			if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+			else if (Character.isUpperCase(x)) answer += Character.toLowerCase(x);
+			else answer += x;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Problem2 T = new Problem2();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
