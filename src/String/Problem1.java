313c976df52f3bfad6457604e1fc426e3a63d9b0
ybchar <uiurihappy@sju.ac.kr>
2023-01-17 12:36:52 +0900

    feat: problem1 solve

--- src/String/Problem1.java
+++ src/String/Problem1.java
@@ -7,13 +7,23 @@ class Main {
 	public int solution(String str, char t){
 		int answer = 0;
 
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+
+		for(int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == t) {
+				answer++;
+			}
+		}
+
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		System.out.println(1);
-		System.out.println();
+		String str = kb.next();
+		char t = kb.next().charAt(0);
+		System.out.println(T.solution(str, t));
 	}
 }
