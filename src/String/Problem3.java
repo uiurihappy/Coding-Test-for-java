05356ae9dc94ae71cc4a10e9997b5ce7d85d7b82
ybchar <uiurihappy@sju.ac.kr>
2023-01-18 11:22:07 +0900

    feat: Problem3 solve

--- src/String/Problem3.java
+++ src/String/Problem3.java
@@ -1,26 +1,34 @@
 package String;
 
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 class Problem3 {
 
 	public String solution(String str) {
-		String[] subStr = str.split(" ");
-
-		Optional<String> max = Arrays.stream(subStr).max( (a, b) -> {
-			return Integer.compare(a.length(), b.length());
-		});
-		System.out.println(max);
-		return max.get();
+//		String[] subStr = str.split(" ");
+//
+//		Optional<String> max = Arrays.stream(subStr).max( (a, b) -> {
+//			return Integer.compare(a.length(), b.length());
+//		});
+//		return max.get();
+		String answer = "";
+		int max = Integer.MIN_VALUE;
+		String[] s = str.split(" ");
+		for(String x : s) {
+			int len = x.length();
+			if(len > max) {
+				max = len;
+				answer = x;
+			}
+		}
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		Problem3 T = new Problem3();
 
 		Scanner kb = new Scanner(System.in);
-		String str = kb.next();
+		String str = kb.nextLine();
 
 		System.out.println(T.solution(str));
 	}
