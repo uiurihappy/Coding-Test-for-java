e51977da485253222d71bdf3b59cde1894f03410
ybchar <uiurihappy@sju.ac.kr>
2023-01-18 12:13:30 +0900

    feat: Problem3 solve 1,2,3

--- src/String/Problem3.java
+++ src/String/Problem3.java
@@ -5,12 +5,18 @@ import java.util.*;
 class Problem3 {
 
 	public String solution(String str) {
-//		String[] subStr = str.split(" ");
-//
-//		Optional<String> max = Arrays.stream(subStr).max( (a, b) -> {
-//			return Integer.compare(a.length(), b.length());
-//		});
-//		return max.get();
+		/*
+		* solution 1
+		String[] subStr = str.split(" ");
+
+		Optional<String> max = Arrays.stream(subStr).max( (a, b) -> {
+			return Integer.compare(a.length(), b.length());
+		});
+		return max.get();
+		 */
+
+		/*
+		* solution 2
 		String answer = "";
 		int max = Integer.MIN_VALUE;
 		String[] s = str.split(" ");
@@ -22,12 +28,32 @@ class Problem3 {
 			}
 		}
 		return answer;
+		*/
+
+		String answer = "";
+
+		int m = Integer.MIN_VALUE, pos;
+
+		// 띄어쓰기의 위치를 return 받는다.
+		while((pos = str.indexOf(' ')) != -1){
+			String temp = str.substring(0, pos); // it, is, time, to, study 루프만큼 저장
+
+			int len = temp.length();
+
+			if(len > m) {
+				m = len;
+				answer = temp;
+			}
+		}
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		Problem3 T = new Problem3();
 
 		Scanner kb = new Scanner(System.in);
+		// 한 줄을 입력 받아야 하니 nextLine으로 받는다.
 		String str = kb.nextLine();
 
 		System.out.println(T.solution(str));
