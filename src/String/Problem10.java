4ac13e26450878c94dd69c1dccf2e1d51728602d
ybchar <uiurihappy@sju.ac.kr>
2023-01-21 13:02:36 +0900

    feat: Problem 10, 11 solve and refactoring

--- src/String/Problem10.java
+++ src/String/Problem10.java
@@ -1,14 +1,42 @@
 package String;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+/*
+	input: 	teachermode e
+	output	1 0 1 2 1 0 1 2 2 1 0
+ */
 
 class Problem10 {
 
 	public int[] solution(String str, char t) {
 		int[] answer = new int[str.length()];
+		int p = 1000;
 
+		// 왼쪽으로부터 떨어진 거리 계산
+		for (int i =0; i < str.length(); i++){
+			if (str.charAt(i) == t) {
+				p = 0;
+				answer[i] = p;
+			} else {
+				p++;
+				answer[i] = p;
+			}
+		}
+		// init p
+		p = 1000;
 
+		for (int i = str.length() - 1; i >= 0; i--){
+			if (str.charAt(i) == t) {
+				p = 0;
+			} else {
+				p++;
+
+				// 둘 중 작은 값으로 대입
+				answer[i] = Math.min(answer[i], p);
+			}
+		}
+
+		// 신기하네... 다시 풀어보자
 		return answer;
 	}
 
@@ -17,9 +45,10 @@ class Problem10 {
 		Scanner kb = new Scanner(System.in);
 
 		String str = kb.next();
+		// 문자 한개를 읽을 때는 이렇게 한다.
 		char t = kb.next().charAt(0);
 		for(int x : T.solution(str, t)) {
-			System.out.println(x + " ");
+			System.out.print(x + " ");
 		}
 	}
 }
--- src/String/Problem11.java
+++ src/String/Problem11.java
@@ -1,35 +1,59 @@
 package String;
 
 import java.util.*;
+/*
+	input1: 	KKHSSSSSSSE
+	output1:	K2HS7E
 
+	input2: 	KSTTTSEEKFKKKDJJGG
+	output2: 	KST3SE2KFK3DJ2G2
+ */
 class Problem11 {
 
-	public String solution(String str) {
-		StringBuilder answer = new StringBuilder();
-		char[] s = str.toCharArray();
-		int cnt = 1;
-		char temp = s[0];
+//	  // solution 1
+//	  public String solution(String str) {
+//		StringBuilder answer = new StringBuilder();
+//		char[] s = str.toCharArray();
+//		int cnt = 1;
+//		char temp = s[0];
+//
+//		for(int i = 1; i < s.length; i++){
+//			if (temp == s[i]) cnt++;
+//			else {
+//				if (cnt != 1) {
+//					answer.append(temp).append(cnt);
+//				}else{
+//					answer.append(temp);
+//				}
+//				temp = s[i];
+//				cnt = 1;
+//			}
+//		}
+//
+//		if (cnt != 1) {
+//			answer.append(temp).append(cnt);
+//		}else{
+//			answer.append(temp);
+//		}
+//
+//		return answer.toString();
+//	}
 
-		for(int i = 1; i < s.length; i++){
-			if (temp == s[i]) cnt++;
-			else {
-				if (cnt != 1) {
-					answer.append(temp).append(cnt);
-				}else{
-					answer.append(temp);
-				}
-				temp = s[i];
+	public String solution (String str) {
+		String answer = "";
+		str = str + " ";
+		int cnt = 1;
+		for(int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) == str.charAt(i+1)) {
+				cnt++;
+			}else{
+				answer += str.charAt(i);
+				if (cnt > 1) answer += String.valueOf(cnt);
 				cnt = 1;
 			}
 		}
 
-		if (cnt != 1) {
-			answer.append(temp).append(cnt);
-		}else{
-			answer.append(temp);
-		}
-
-		return answer.toString();
+		return answer;
 	}
 
 	public static void main(String[] args) {
