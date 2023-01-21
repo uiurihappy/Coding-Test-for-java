38c070a92748ef852382fb72aacd8014df400c25
ybchar <uiurihappy@sju.ac.kr>
2023-01-21 15:19:36 +0900

    feat: Array Problem 1 solve

---
+++ src/ArrayProblem/Problem1.java
@@ -0,0 +1,31 @@
+package ArrayProblem;
+
+import java.util.*;
+
+class Problem1 {
+
+    public ArrayList<Integer> solution (int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] < arr[i]) {
+                answer.add(arr[i]);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem1 T = new Problem1();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
