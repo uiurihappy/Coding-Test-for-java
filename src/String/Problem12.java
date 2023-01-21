b6ea207bb7de7b59dc0d448ea15c5fc2551f14c1
ybchar <uiurihappy@sju.ac.kr>
2023-01-21 13:57:35 +0900

    feat: Problem 12 refactoring

--- src/String/Problem12.java
+++ src/String/Problem12.java
@@ -4,17 +4,31 @@ import java.util.Scanner;
 
 class Problem12 {
 
-	public String solution(int n, String str){
-		StringBuilder answer = new StringBuilder();
+//	public String solution(int n, String str){
+//		StringBuilder answer = new StringBuilder();
+//
+//		for (int i = 0; i < n * 7; i += 7) {
+//			String numString = str.substring(i, i + 7);
+//			int binary = Integer.parseInt(numString.replaceAll("#", "1").replaceAll("*", "0"));
+//			char temp =(char) Integer.parseInt(String.valueOf(binary), 2);
+//			answer.append(temp);
+//		}
+//
+//		return answer.toString();
+//	}
 
-		for (int i = 0; i < n * 7; i += 7) {
-			String numString = str.substring(i, i + 7);
-			int binary = Integer.parseInt(numString.replaceAll("#", "1").replaceAll("[*]", "0"));
-			char temp =(char) Integer.parseInt(String.valueOf(binary), 2);
-			answer.append(temp);
+	public String solution(int n, String str) {
+		String answer = "";
+
+		for (int i = 0; i < n; i++) {
+			String temp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+			int num = Integer.parseInt(temp, 2);
+			answer += (char) num;
+
+			str = str.substring(7);
 		}
 
-		return answer.toString();
+		return answer;
 	}
 
 	public static void main(String[] args) {
