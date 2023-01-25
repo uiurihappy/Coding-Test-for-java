01915d62fcbfb1dc3bb62190eb31956002aa7be0
ybchar <uiurihappy@sju.ac.kr>
2023-01-25 14:36:31 +0900

    feat: Problem6 solve

--- src/ArrayProblem/Problem6.java
+++ src/ArrayProblem/Problem6.java
@@ -4,37 +4,32 @@ import java.util.*;
 
 class Problem6 {
 
-	// 소수 체크
 	public boolean isPrime(int num) {
-		if (num == 1) return false;
+
+		if (num < 2) return false;
 
 		for (int i = 2; i < num; i++) {
-			if (num % i == 0) // 소수가 아니다.
-				return false;
+			if (num % i == 0) return false;
 		}
 
 		return true;
 	}
 
-	public ArrayList<Integer> solution(int n, int[] arr) {
+
+	public ArrayList<Integer> solution (int[] arr) {
 		ArrayList<Integer> answer = new ArrayList<>();
 
-		for (int i = 0; i < n; i++) {
+		// 숫자 뒤집기
+		for (int j : arr) {
+			int temp = j;
 			int res = 0;
-			int temp = arr[i];
-			// ex) temp = 123
+			// temp = 123
 			while (temp > 0) {
-				// t = 123 % 10 = 3
 				int t = temp % 10;
-				// res = 0 + 3 = 3
 				res = res * 10 + t;
-				// temp = 123 / 10 = 12, 12 / 10 = 1
 				temp = temp / 10;
 			}
-
-			if (isPrime(res)) {
-				answer.add(res);
-			}
+			if (isPrime(res)) answer.add(res);
 		}
 
 
@@ -46,11 +41,12 @@ class Problem6 {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			arr[i] = kb.nextInt();
 		}
 
-		for(int x : T.solution(n, arr)) {
+		for (int x : T.solution(arr)) {
 			System.out.print(x + " ");
 		}
 
