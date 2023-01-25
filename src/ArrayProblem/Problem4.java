52bc8a2481e71d4bd2323156621a42b9889d63d0
ybchar <uiurihappy@sju.ac.kr>
2023-01-25 11:23:19 +0900

    feat: Problem4 피보나치 재귀

--- src/ArrayProblem/Problem4.java
+++ src/ArrayProblem/Problem4.java
@@ -3,7 +3,7 @@ package ArrayProblem;
 import java.util.Scanner;
 
 class Problem4 {
-
+//      solution 1
     public void solution (int n) {
 
         int a = 1, b = 1, c;
@@ -25,4 +25,20 @@ class Problem4 {
         int n = kb.nextInt();
         T.solution(n);
     }
+
+    // solution 2 (run time error)
+//    public int solution (int n) {
+//        if (n <= 1) {
+//            return n;
+//        } else {
+//            return solution(n - 2) + solution(n - 1);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Problem4 T = new Problem4();
+//        Scanner kb = new Scanner(System.in);
+//        int n = kb.nextInt();
+//        for(int i = 1; i <= n; i++) System.out.print(T.solution(i) + " ");
+//    }
 }
