9085af7707b842d29f6cc11379756f71a7ad3181
ybchar <uiurihappy@sju.ac.kr>
2023-01-29 12:50:20 +0900

    feat: Problem 5 solve

--- src/Chapter3/Problem5.java
+++ src/Chapter3/Problem5.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 class Problem5 {
 
     public int solution (int n) {
-        int answer = 0;
+        int answer = 0, sum = 0, lt = 0;
+        int m = n / 2 + 1; // ex) 8 = 15 / 2 + 1
+        int[] arr = new int[m];
 
-        for (int i = 0; i < n; i++) {
-            int m = 0;
-            while (m <= n) {
-                if (m == n)
-                    answer++;
-                m += i;
+        for (int i = 0; i < m; i++)
+            arr[i] = i + 1;
+
+        for (int rt = 0; rt < m; rt++) {
+            sum += arr[rt];
+            if(sum == n) answer++;
+
+            while (sum >= n) {
+                sum -= arr[lt++];
+                if (sum == n) answer++;
             }
         }
+
         return answer;
     }
 
