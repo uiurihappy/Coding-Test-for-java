a4f0e21b43a86e968d5506443b4b69dd799ab991
ybchar <uiurihappy@sju.ac.kr>
2023-01-31 15:05:52 +0900

    feat: Problem 2 solve

---
+++ src/Chapter4/Problem2.java
@@ -0,0 +1,31 @@
+package Chapter4;
+
+import java.util.*;
+
+class Problem2 {
+
+	public String solution(String str1, String str2) {
+		String answer = "NO";
+		HashMap<Character, Integer> map1 = new HashMap<>();
+		HashMap<Character, Integer> map2 = new HashMap<>();
+
+		for (char x : str1.toCharArray()) {
+			map1.put(x, map1.getOrDefault(x, 0) + 1);
+		}
+
+		for (char x : str2.toCharArray()) {
+			map2.put(x, map2.getOrDefault(x, 0) + 1);
+		}
+		if (map1.equals(map2)) return "YES";
+
+		return answer;
+	}
+	public static void main(String[] args) {
+		Problem2 T = new Problem2();
+		Scanner kb = new Scanner(System.in);
+		String str1 = kb.next();
+		String str2 = kb.next();
+
+		System.out.println(T.solution(str1, str2));
+	}
+}
