1105458d880eaef7c9db13913ca1bfb6000b3bce
ybchar <uiurihappy@sju.ac.kr>
2023-02-01 16:56:53 +0900

    feat: Problem 1 solve

---
+++ src/Chapter5/Problem1.java
@@ -0,0 +1,28 @@
+package Chapter5;
+
+import java.util.*;
+
+class Problem1 {
+
+	public String solution(String str) {
+		String answer = "YES";
+		Stack<Character> stack = new Stack<>();
+		for (char x : str.toCharArray()) {
+			if (x == '(') stack.push(x);
+			else {
+				if(stack.isEmpty()) return "NO";
+				stack.pop();
+			}
+		}
+		if (!stack.isEmpty()) return "NO";
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Problem1 T = new Problem1();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
