8a5c5c9628f568f75054054a59372236fb0e353c
ybchar <uiurihappy@sju.ac.kr>
2023-02-05 23:25:34 +0900

    feat: Problem 5 solving

---
+++ src/Chapter5/Problem5.java
@@ -0,0 +1,19 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+class Problem5 {
+
+    public int solution (String str) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem5 T = new Problem5();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
