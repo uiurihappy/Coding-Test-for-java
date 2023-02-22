b5232e5f2c4e76e9f31166f494686249b7ee5527
ybchar <uiurihappy@sju.ac.kr>
2023-02-22 15:31:12 +0900

    feat: Problem 7 comment

--- src/Chapter6/Problem7.java
+++ src/Chapter6/Problem7.java
@@ -2,6 +2,22 @@ package Chapter6;
 
 import java.util.*;
 
+/**
+ * 좌표 정렬
+ * input
+5
+2 7
+1 3
+1 2
+2 5
+3 6
+ * output
+1 2
+1 3
+2 5
+2 7
+3 6
+ */
 class Point implements Comparable<Point> {
 	public int x, y;
 
@@ -10,14 +26,16 @@ class Point implements Comparable<Point> {
 		this.y = y;
 	}
 
+	// sort 함수의 정렬 기준
 	@Override
 	public int compareTo(Point o) {
 		if (this.x == o.x) {
-			System.out.println("x Compare = " + (this.y - o.y));
+//			System.out.println("x Compare = " + (this.y - o.y));
+			// 오름차순: this (현 메소드 객체) 뒤에 o (받아온 객체) 정렬될려면 음수값으로 return 받아야 한다.
 			return this.y - o.y;
 		}
 		else {
-			System.out.println("y Compare = " + (this.x - o.y));
+//			System.out.println("y Compare = " + (this.x - o.y));
 			return this.x - o.x;
 		}
 	}
