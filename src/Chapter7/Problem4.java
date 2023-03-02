9ca71edbd58fae453ae6e6b7165813b86f13c196
ybchar <uiurihappy@sju.ac.kr>
2023-03-02 15:55:15 +0900

    feat: static instance practice

--- src/Chapter7/Problem4.java
+++ src/Chapter7/Problem4.java
@@ -2,6 +2,11 @@ package Chapter7;
 
 import java.util.Scanner;
 
+class Number {
+	static int num = 0;
+	int num2 = 0;
+}
+
 /**
  * 피보나치 수열
  * input
@@ -28,5 +33,21 @@ class Problem4 {
 		for (int i = 1; i <= n; i++) {
 			System.out.print(T.solution(i) + " ");
 		}
+
+		System.out.println();
+
+		Number number1 = new Number();
+		Number number2 = new Number();
+
+		// 클래스에 고정되어 있는 값
+		//		Number.num++; 와 동일
+		number1.num++;
+		number2.num2++;
+
+		// 클래스 변수이므로 Number 클래스에 종속
+		System.out.println("number2 = " + number2.num);
+		// 인스턴스 변수이므로 main 클래스에 종속
+		System.out.println("number2 = " + number2.num2);
+
 	}
 }
