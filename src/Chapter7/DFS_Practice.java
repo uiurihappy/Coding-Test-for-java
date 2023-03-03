325fbc5ce50302d1d4fc3bf29a5eb27a4ba40d4c
ybchar <uiurihappy@sju.ac.kr>
2023-03-03 16:16:23 +0900

    feat: binary-tree Practice

--- src/Chapter7/DFS_Practice.java
+++ src/Chapter7/DFS_Practice.java
@@ -6,6 +6,7 @@ package Chapter7;
  * 중위 순회: 왼쪽 자식부터 방문하고 부모 -> 오른쪽 자식
  * 후위 순회: 왼쪽 자식부터 방문하고 오른쪽 자식 -> 부모
  *
+ * 말단 노드의 값은 null
  */
 
 class Node {
@@ -19,7 +20,31 @@ class Node {
 
 public class DFS_Practice {
 	Node root;
-	public void DFS (Node root) {
+	public void DFS (Node root, String orderKey) {
+
+		// 말단 노드일 때,
+		if (root == null) return;
+		else {
+			switch (orderKey) {
+				case "pre":
+					// 전위 순회 (pre-order)
+					System.out.print(root.data + " ");
+					DFS(root.lt, orderKey);
+					DFS(root.rt, orderKey);
+					break;
+				case "in":
+					// 중위 순회 (in-order)
+					DFS(root.lt, orderKey);
+					System.out.print(root.data + " ");
+					DFS(root.rt, orderKey);
+					break;
+				case "post":
+					// 후위 순회 (post-order)
+					DFS(root.lt, orderKey);
+					DFS(root.rt, orderKey);
+					System.out.print(root.data + " ");
+			}
+		}
 
 	}
 	public static void main(String[] args) {
@@ -33,8 +58,12 @@ public class DFS_Practice {
 		tree.root.lt.lt = new Node(4);
 		tree.root.lt.rt = new Node(5);
 		tree.root.rt.lt = new Node(6);
-		tree.root.lt.rt = new Node(7);
-
-
+		tree.root.rt.rt = new Node(7);
+		String[] orderKey = {"pre", "in", "post"};
+		for (String key : orderKey) {
+			System.out.println(key + "-order");
+			tree.DFS(tree.root, key);
+			System.out.println();
+		}
 	}
 }
