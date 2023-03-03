2f6ad31111bb8f1b7d75912c92421dda7877fcc3
ybchar <uiurihappy@sju.ac.kr>
2023-03-04 02:51:18 +0900

    feat: Problem 5 solve

--- src/Chapter7/Problem5.java
+++ src/Chapter7/Problem5.java
@@ -19,8 +19,21 @@ public class Problem5 {
 	static int n;
 	public void solution(int L) {
 		if (L == (n + 1)) {
+			String tmp = "";
 
+			for(int i = 1; i <= n; i++) {
+				if (ch[i] == 1) tmp += (i + " ");
+			}
+			// 공집합 예외 처리
+			if (tmp.length() > 0) System.out.println(tmp);
 		} else {
+			ch[L] = 1; 	// 사용한다.
+			// 왼쪽
+			solution(L+1);
+
+			ch[L] = 0; 	// 사용하지 않는다.
+			// 오른쪽
+			solution(L+1);
 
 		}
 	}
@@ -30,6 +43,6 @@ public class Problem5 {
 		Scanner kb = new Scanner(System.in);
 		n = kb.nextInt();
 		ch = new int[n + 1];
-		T.solution(n);
+		T.solution(1);
 	}
 }
