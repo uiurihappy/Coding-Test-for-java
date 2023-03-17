c3ae7734f88bf1931e2eff37221ca5fe5539ee15
ybchar <uiurihappy@sju.ac.kr>
2023-03-17 17:52:37 +0900

    feat: Safety zone solve

--- src/Programmers/SafetyZone.java
+++ src/Programmers/SafetyZone.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * output1
  16
  * input2
+ 5
  0 0 0 0 0
  0 0 0 0 0
  0 0 0 0 0
@@ -26,30 +27,39 @@ public class SafetyZone {
 
 	static int n, answer = 0;
 	static int[][] board;
-	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
 
-	public void DFS(int x, int y, int[][] board) {
-		for (int i = 0; i < 8; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
 
-			if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1){
-				board[nx][ny] += 1;
-				DFS(nx, ny, board);
-			}
-		}
-	}
-
-	public void solution(int[][] board) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+	public int solution(int[][] board) {
+		SafetyZone T = new SafetyZone();
+		int[][] boomBoard = new int[n][board[0].length];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j] == 1) {
-					DFS(i, j, board);
+					T.Explosion(i, j, boomBoard);
 				}
 			}
 		}
 
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++) {
+				if (boomBoard[i][j] <= 0) answer++;
+				System.out.print(boomBoard[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return answer;
+	}
+	public void Explosion(int x, int y, int[][] boomBoard) {
+		int[] dx = {0, -1, -1, -1, 0, 0, 1, 1, 1};
+		int[] dy = {0, -1, 0, 1, -1, 1, -1, 0, 1};
+		for (int i = 0; i < 9; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n){
+				boomBoard[nx][ny] = 1;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
@@ -62,14 +72,7 @@ public class SafetyZone {
 			for (int j = 0; j < n; j++)
 				board[i][j] = kb.nextInt();
 
-		T.solution(board);
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < n; j++) {
-				if (board[i][j] <= 0) answer++;
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println(answer);
+		System.out.println(T.solution(board));
+
 	}
 }
