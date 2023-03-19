d7f661c0cc10f99a1f044b9f8de4ead9e4eccb25
ybchar <uiurihappy@sju.ac.kr>
2023-03-19 14:47:34 +0900

    feat: Programmers OX quiz

--- src/Programmers/OX_Quiz.java
+++ src/Programmers/OX_Quiz.java
@@ -2,6 +2,20 @@ package Programmers;
 
 import java.util.Scanner;
 
+/** OX퀴즈
+ * input1
+3 - 4 = -3
+5 + 6 = 11
+ * output1
+ X O
+ * input2
+ 19 - 6 = 13
+ 5 + 66 = 71
+ 5 - 15 = 63
+ 3 - 1 = 2
+ * output2
+ O O X O
+ */
 public class OX_Quiz {
 
     static String[] quiz;
@@ -9,8 +23,13 @@ public class OX_Quiz {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
 
-        for (int i = 0; i < quiz.length; i++) {
 
+        for (int i = 0; i < quiz.length; i++) {
+            String[] splitStr = quiz[i].split(" ");
+            int result =
+                    Integer.parseInt(splitStr[0]) +
+                            (Integer.parseInt(splitStr[2]) * (splitStr[1].equals("-") ? -1 : 1));
+            answer[i] = Integer.parseInt(splitStr[4]) == result ? "O" : "X";
         }
 
         return answer;
