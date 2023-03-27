2a015e350f28159fd3b3fb33483b968fb741afea
ybchar <uiurihappy@sju.ac.kr>
2023-03-27 13:28:42 +0900

    feat: Programmers TwoPassword sol 2

--- src/Programmers/TwoPassword.java
+++ src/Programmers/TwoPassword.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 /** 둘만의 암호
  * input1
- aukks
- wbqd
- 5
+aukks
+wbqd
+5
  * output1
  happy
  */
@@ -15,19 +15,34 @@ public class TwoPassword {
 	public String solution(String s, String skip, int index) {
 		String answer = "";
 
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			int cnt = 1;
-			while (cnt <= index) {
-				++c;
+		// sol 1
+//		for (int i = 0; i < s.length(); i++) {
+//			char c = s.charAt(i);
+//			int cnt = 1;
+//			while (cnt <= index) {
+//				++c;
+//
+//				if (c > 'z')
+//					c -= 26;
+//				if (skip.contains(c + ""))
+//					continue;
+//				else cnt++;
+//			}
+//			answer += c;
+//		}
 
-				if (c > 'z')
-					c -= 26;
-				if (skip.contains(c + ""))
-					continue;
-				else cnt++;
+		// sol 2
+		for (char letter : s.toCharArray()) {
+			char temp = letter;
+			int cnt = 0;
+			while (cnt < index) {
+				// 삼항 연산자 사용
+				temp = temp == 'z' ? 'a' : (char) (temp+1);
+				if (!skip.contains(temp + ""))
+					cnt++;
+
 			}
-			answer += c;
+			answer += temp;
 		}
 
 		return answer;
