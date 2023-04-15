e66e8f1f1f16d1fb9278ec852ff6bbf081a45d58
ybchar <uiurihappy@sju.ac.kr>
2023-04-15 15:03:12 +0900

    feat: Programmers CaesarCipher sol 2

--- src/Programmers/CaesarCipher.java
+++ src/Programmers/CaesarCipher.java
@@ -32,6 +32,19 @@ public class CaesarCipher {
             }
         }
         return answer.toString();
+
+        // sol 2
+//        return s.chars().map(c -> {
+//                    int n = _n % 26;
+//                    if (c >= 'a' && c <= 'z') {
+//                        return 'a' + (c - 'a' + n) % 26;
+//                    } else if (c >= 'A' && c <= 'Z') {
+//                        return 'A' + (c - 'A' + n) % 26;
+//                    } else {
+//                        return c;
+//                    }
+//                }).mapToObj(c -> String.valueOf((char)c))
+//                .reduce((a, b) -> a + b).orElse("");
     }
 
     public static void main(String[] args) {
