8ab88e04707e35ba54b1c390d9bce150f7f05487
ybchar <uiurihappy@sju.ac.kr>
2023-04-28 16:08:24 +0900

    feat: Programmers PickTwoAdd

---
+++ src/Programmers/PickTwoAdd.java
@@ -0,0 +1,41 @@
+package Programmers;
+
+import java.util.*;
+
+public class PickTwoAdd {
+
+	public int[] solution(int[] numbers) {
+		ArrayList<Integer> answer = new ArrayList<>();
+
+		// sol 2 (set을 사용해도 됨)
+//		Set<Integer> answer = new HashSet<>();
+//
+//		for(int i=0; i<numbers.length; i++) {
+//			for(int j=i+1; j<numbers.length; j++) {
+//				answer.add(numbers[i] + numbers[j]);
+//			}
+//		}
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				int num = numbers[i] + numbers[j];
+				if (!answer.contains(num)) {
+					answer.add(num);
+				}
+			}
+		}
+		return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+
+	}
+
+	public static void main(String[] args) {
+		PickTwoAdd T = new PickTwoAdd();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] numbers = new int[n];
+		for (int i = 0; i < n; i++)
+			numbers[i] = kb.nextInt();
+		for (int x : T.solution(numbers))
+			System.out.print(x + " ");
+	}
+}
